8a23d377a01b8ffa29fd7c8e5fafab91295ed969
johnathanluong <johnathanluong7@gmail.com>
2024-03-24 18:29:05 -0400

    Setup Amplify Auth + amplify add auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "signin6836814d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,8 @@
 import { Stack } from 'expo-router';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '@/amplifyconfiguration.json';
+
+Amplify.configure(amplifyconfig);
 
 export default function RootLayout() {
 	return <Stack></Stack>;
